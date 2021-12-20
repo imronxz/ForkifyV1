372daad1205863d67a7c8bb61edc7d2ba0e573e1
imronxz <imronxz@gmail.com>
2021-12-20 12:05:38 +0700

    api

--- src/js/config.js
+++ src/js/config.js
@@ -1,5 +1,5 @@
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
-export const KEY = '<YOUR_KEY>';
+export const KEY = 'a9095aab-4f83-485b-aef9-e88ab8a52875';
 export const MODAL_CLOSE_SEC = 2.5;
